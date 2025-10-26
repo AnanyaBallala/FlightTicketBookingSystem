6b30e0f794e2cfbfe353a1b87ca06abc67339c65
AnanyaBallala <ananyaballala@gmail.com>
2025-10-26 21:31:34 +0530

    Add files via upload

---
+++ flights.js
@@ -0,0 +1,31 @@
+document.getElementById("searchForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const from = document.getElementById("from").value;
+  const to = document.getElementById("to").value;
+  const date = document.getElementById("date").value;
+
+  const results = document.getElementById("resultsContainer");
+  results.innerHTML = `
+    <h3>Available Flights from ${from} to ${to} on ${date}</h3>
+    <div class="flight-card">
+      <p>Flight: AI203 | Airline: Air India | Time: 10:00 AM</p>
+      <button onclick="bookFlight('AI203')">Book Now</button>
+    </div>
+    <div class="flight-card">
+      <p>Flight: 6E110 | Airline: Indigo | Time: 12:30 PM</p>
+      <button onclick="bookFlight('6E110')">Book Now</button>
+    </div>
+  `;
+});
+
+function bookFlight(flightId) {
+ 
+  localStorage.setItem("selectedFlight", flightId);
+  localStorage.setItem("fromCity", document.getElementById("from").value);
+  localStorage.setItem("toCity", document.getElementById("to").value);
+  localStorage.setItem("flightDate", document.getElementById("date").value);
+
+  
+  window.location.href = "booking.html";
+}
